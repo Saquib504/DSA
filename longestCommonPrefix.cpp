21d25c354da9c98e4e389d9a91bc3d63920153e7
Saquib504 <saquibwound@gmail.com>
2025-08-22 17:46:17 +0530

    added longestCommonPrefix.cpp

---
+++ longestCommonPrefix.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    Node*list[26];
+    bool flag = false;
+    int cntPrefix = 0;
+
+    bool containsKey(char ch) {
+        return list[ch - 'a'] != NULL;
+    }
+
+    void put(char ch, Node* node) {
+        list[ch - 'a'] = node;
+    }
+
+    Node* get(char ch) {
+        return list[ch - 'a'];
+    }  
+
+    void increasePrefix() {
+        cntPrefix++;
+    }
+
+    void setEnd() {
+        flag = true;
+    }
+
+    bool isEnd() {
+        return flag;
+    }
+};
+
+
+class Trie {
+private:
+    Node* root;
+public:
+    Trie() {
+        root = new Node();
+    }
+
+    void insert(string word) {
+        Node*node = root;
+        for(int i = 0; i < word.length(); i++) {
+            if(!node->containsKey(word[i])){
+                node->put(word[i], new Node());
+            }
+            node = node->get(word[i]);
+            node->increasePrefix();
+        }
+        node->setEnd();
+    }
+
+
+    string longestCommonPrefix(string word, int n) {
+        Node*node = root;
+        string ans = "";
+        for(auto c : word) {
+            if(node->containsKey(c)) {
+                node = node->get(c);
+                if(node->cntPrefix == n) {
+                    ans = ans + c;
+                }
+                else break;
+            } else break;
+        }
+        return ans;
+    }
+};
+
+
+string longestCommonPrefix(vector<string> &arr, int n)
+{
+    // Write your code here
+    auto it = max_element(arr.begin(), arr.end(),
+                          [](const string &a, const string &b) {
+                              return a.size() < b.size();
+                          });
+
+    Trie trie;
+    for(int i = 0; i < arr.size(); i++) {
+        trie.insert(arr[i]);
+    }
+
+    return trie.longestCommonPrefix(*it, n);
+}
+
+
+
+
+
+int main() {
+
+    vector<string> arr = {"coder", "codingninja", "coding","code","codable"};
+    cout << longestCommonPrefix(arr, arr.size()) << endl;
+
+    return 0;
+}
