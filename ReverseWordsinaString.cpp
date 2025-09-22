448bd3b23dcc3983edbd5405daaae3deed40d01a
Saquib504 <saquibwound@gmail.com>
2025-09-22 23:57:55 +0530

    added ReverseWordsinaString.cpp

---
+++ ReverseWordsinaString.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <sstream>  // <-- for istringstream
+#include <vector>   // <-- for vector
+using namespace std;
+
+string reverseWords(string s) {
+    istringstream iss(s);
+    vector<string> words;
+    string word;
+
+    while (iss >> word) {
+        words.push_back(word);
+    }
+
+    int left = 0, right = words.size() - 1;
+    while (left < right) {
+        swap(words[left], words[right]);
+        left++;
+        right--;
+    }
+
+    string result;
+    for (const string& w : words) {
+        if (!result.empty()) {
+            result += " ";
+        }
+        result += w;
+    }
+
+    return result;
+}
+
+int main() {
+    string test_case = "the morning is beautiful";
+
+    string ans = reverseWords(test_case);
+    cout << ans << endl;
+}
