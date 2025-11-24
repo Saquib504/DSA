6b4149e5408245ce7be34281abf5d6cfe7637dca
Saquib504 <saquibwound@gmail.com>
2025-11-24 15:28:21 +0530

    added convert2Decimal() function

--- bitmanipulationintroduction.cpp
+++ bitmanipulationintroduction.cpp
@@ -23,11 +23,26 @@ string convert2Binary(int n) {
     return result;
 }
 
+string convert2Decimal(string s) {
+    int result = 0;
+    int power = 1; // 2^0
+
+    for(int i = s.length() - 1; i >= 0; i--) {
+        if(s[i] == '1') {
+            result += power;
+        }
+        power = power * 2;
+    }
+
+    return to_string(result);
+}
+
 
 int main() {
     int n; cout << "Enter an integer to be converted into it's binary form: ";cin >> n;
     string s; cout << "Enter the binary number to be converted into it's decimal form: ";cin >>s;
 
     cout << "Your decimal to binary conversion is :" << convert2Binary(n) << endl;
+    cout << "Your binary to decimal conversion is :" << convert2Decimal(s) << endl;
     
 }
