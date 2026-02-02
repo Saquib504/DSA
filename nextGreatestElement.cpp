17cefd1aa10e6aecd72295aef6120990df5694f3
Saquib504 <saquibwound@gmail.com>
2026-02-02 12:05:47 +0530

    made some changes to the nextGreaterElement.cpp

--- nextGreatestElement.cpp
+++ nextGreatestElement.cpp
@@ -6,23 +6,23 @@ using namespace std;
 
 vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
 
-    unordered_map<int, int> mp;   // value -> next greater
-    stack<int> st;
+    unordered_map<int, int> map;
+    stack<int> stk;
 
-    // Process nums2
-    for (int i = nums2.size() - 1; i >= 0; i--) {
-        while (!st.empty() && st.top() <= nums2[i]) {
-            st.pop();
+    for(int i = nums2.size() - 1; i >= 0; i--) {
+        while(!stk.empty() && stk.top() <= nums2[i]) {
+            stk.pop();
         }
 
-        mp[nums2[i]] = st.empty() ? -1 : st.top();
-        st.push(nums2[i]);
+        map[nums2[i]] = stk.empty() ? -1 : stk.top();
+
+        stk.push(nums2[i]);
     }
 
-    // Build result for nums1
     vector<int> res(nums1.size());
-    for (int i = 0; i < nums1.size(); i++) {
-        res[i] = mp[nums1[i]];
+
+    for(int i = 0; i < nums1.size(); i++ ){
+        res[i] = map[nums1[i]];
     }
 
     return res;
