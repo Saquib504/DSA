397f746a248778274a7178b8d432891f84848cd7
Saquib504 <saquibwound@gmail.com>
2026-02-03 10:20:33 +0530

    added one more similar function to nextSmallerElement

--- nextSmallerElement.cpp
+++ nextSmallerElement.cpp
@@ -21,10 +21,33 @@ vector<int> nextSmallerElement(vector<int> &nums) {
 }
 
 
+
+
+
+
+vector<int> nextSmaller(vector<int> &nums) {
+    int n = nums.size();
+    vector<int> ans(n);
+
+    stack<int> stk;
+
+    for(int i = n-1; i >= 0; i--) {
+        while(!stk.empty() && stk.top() >= nums[i]) {
+            stk.pop();
+        }
+
+        ans[i] = stk.empty() ? -1 : stk.top();
+        stk.push(nums[i]);
+    }
+
+    return ans;
+}
+
+
 int main() {
     vector<int>nums = {4,8,5,2,25};
 
-    vector<int> res = nextSmallerElement(nums);
+    vector<int> res = nextSmaller(nums);
 
     for(auto num : res) {
         cout << num << " ";
