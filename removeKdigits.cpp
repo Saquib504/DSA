2c7d73bbf9559dfd58d15bf992e62a7e987086e1
Saquib504 <saquibwound@gmail.com>
2026-02-13 08:18:39 +0530

    added removeKdigits.cpp

---
+++ removeKdigits.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+
+string removeKdigits(string num, int k) {
+    string ans="";// this will represent the stack 
+    for(auto ch: num){
+        while(ans.size() && ch < ans.back() && k){
+            ans.pop_back();
+            k--;
+        }
+        if(ans.size()==0 && ch=='0')continue;
+        ans.push_back(ch);
+    }
+
+    while(ans.size() && k--)ans.pop_back();
+
+    if(ans.size()>0)return ans;
+    return "0";
+}
+
+
+int main() {
+    string num = "1432219";
+    int k = 3;
+
+    cout << removeKdigits(num, k) << endl;
+
+    return 0;
+}
