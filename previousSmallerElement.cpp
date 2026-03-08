a2d82b874cfdb722b3915d3c2e40da6ef3d034c1
Saquib504 <saquibwound@gmail.com>
2026-03-09 03:24:33 +0530

    added previousSmallerElement.cpp

---
+++ previousSmallerElement.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+
+vector<int> findPSE(vector<int> &nums) {
+    int n = nums.size();
+    vector<int> ans(n);
+    stack<int> stk;
+
+    for(int i = 0; i < n; i++) {
+
+        while(!stk.empty() && stk.top() >= nums[i]) {
+            stk.pop();
+        }
+
+        ans[i] = stk.empty() ? -1 : stk.top();
+        stk.push(nums[i]);
+    }
+    return ans;
+}
+
+int main() {
+    vector<int> nums = {4,12,5,3,1,2,5,3,1,2,4,6};
+    vector<int> res = findPSE(nums);
+    for(auto num : res) {
+        cout << num << " ";
+    }cout << endl;
+    return 0;
+}
