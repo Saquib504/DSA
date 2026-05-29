a2343098b4c1b372dc7d7c2fb9d1cf6a18834515
Saquib504 <saquibwound@gmail.com>
2026-05-29 18:53:45 +0530

    added my solution, solved using hashmap

--- lc3.cpp
+++ lc3.cpp
@@ -23,10 +23,33 @@ using namespace std;
         return maxLen;
     }
 
+    int lengthOfSubstring(string s) {
+        int n = s.size(), l = 0, r = 0, maxLen = 0;
+        int hash[26];
+        fill(hash, hash+26, -1);
+
+        while(r < n) {
+            if(hash[s[r]-'a'] == -1) hash[s[r]-'a'] = 1;
+            else {
+                while(s[l]-'a' != s[r]-'a') {
+                    hash[s[l]-'a'] = -1;
+                    l++;
+                }
+                hash[s[l]-'a'] = -1;
+                hash[s[r]-'a'] = 1;
+                l++;
+            }
+            maxLen = max(maxLen, r-l+1);
+            r++;
+        }
+        return maxLen;
+    }
+
 
 int main() {
-    string s = "au";
-    int result = lengthOfLongestSubstring(s);
-    cout << result << endl;
+    string s;
+    cin >> s;
+    cout << lengthOfLongestSubstring(s) << endl;
+    cout << lengthOfSubstring(s) << endl;
     return 0;
 }
