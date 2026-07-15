6640894e991ee6df0a4f1d50aac7dfbba1a8a790
Saquib504 <saquibwound@gmail.com>
2026-07-15 15:30:48 +0530

    GCD of Odd and Even Sums

---
+++ lc3658.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int gcdOfOddEvenSums(int n) {
+    return n;
+}
+
+
+int main() {
+    int n; cout << "Enter n: "; cin >> n;
+
+    cout << "The GCD of evenSum and oddSum of numbers from 1 to n : " << gcdOfOddEvenSums(n) << endl;
+    return 0;
+}
