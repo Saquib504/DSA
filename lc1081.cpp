677b474820c4e020dc6ed38d31021f1f03ef3192
Saquib504 <saquibwound@gmail.com>
2026-07-19 12:17:41 +0530

    Smallest Subsequence of Distinct Characters

---
+++ lc1081.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+string smallestSubsequence(string s) {
+    int freq[27] = {0};
+    bitset<27> seen;
+    string stk;
+
+    for(auto& c : s) {
+        freq[c&31]++;
+    }
+
+    for(auto& c : s) {
+        int x = c & 31;
+
+        freq[x]--;
+
+        if(seen.test(x)) continue;
+
+        while(stk.length() && stk.back() > c && freq[stk.back()&31]) {
+            seen.reset(stk.back()&31);
+            stk.pop_back();
+        }
+
+        stk.push_back(c);
+        seen.set(x);
+    }
+    return stk;
+}
+
+
+int main() {
+    string s; cout << "Enter the string: "; cin >> s;
+    cout << "Smallest subsequnce with distinct letters: " << smallestSubsequence(s) << endl;
+
+    return 0;
+}
