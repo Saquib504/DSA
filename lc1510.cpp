be327a7319ccc18352247b8a5461864e783978eb
Saquib504 <saquibwound@gmail.com>
2026-08-10 16:58:45 +0530

    Stone Game IV

---
+++ lc1510.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+
+static constexpr int MAX = 100000;
+inline static bitset<MAX+1> dp;
+
+inline static bool init = []() {
+    for(int i = 0; i < MAX; i++) {
+        if(dp.test(i)) continue;
+        for(int j = 1; j*j <= MAX-i; j++) {
+            dp.set(i + j*j);
+        }
+    }
+
+    return false;
+}();
+
+bool winnerSquareGame(int n) {
+    return dp.test(n);
+}
+
+
+int main() {
+    int n; cin >> n;
+
+    if(winnerSquareGame(n)) {
+        cout << "Alice wins!" << endl;
+    } else {
+        cout << "Bob wins!" << endl;
+    }
+
+    return 0;
+}
